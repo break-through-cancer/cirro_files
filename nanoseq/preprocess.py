94485a3d38640c2b03b3e23b57b36ff717def23e
nickibeaks <buechler.nicole@gmail.com>
2024-09-17 15:59:13 -0400

    adding tocsv for samplesheet

--- nanoseq/preprocess.py
+++ nanoseq/preprocess.py
@@ -62,4 +62,4 @@ if __name__ == "__main__":
     ds = PreprocessDataset.from_running()
     setup_inputs(ds)
     setup_options(ds)
-    ds[['group','replicate','barcode','input_file','fasta','gtf']].samplesheet.to_csv("samplesheet.csv", index=False)
+    ds.samplesheet[['group','replicate','barcode','input_file','fasta','gtf']].to_csv("samplesheet.csv", index=False)
