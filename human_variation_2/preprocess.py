dd86830f79da1262fc3c263dcf0bc8ef064d80dc
dltamayo <DLT@breakthroughcancer.org>
2024-11-18 13:47:32 -0500

    Add input_type param removal

--- human_variation_2/preprocess.py
+++ human_variation_2/preprocess.py
@@ -1,11 +1,13 @@
 from cirro.helpers.preprocess_dataset import PreprocessDataset
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ds = PreprocessDataset.from_running()
 
-    if ds.params.get('input_type') == 'file':
-        ds.add_param('bam', ds.params.get('bam_file'))
+    if ds.params.get("input_type") == "file":
+        ds.add_param("bam", ds.params.get("bam_file"))
         ds.remove_param("bam_file")
     else:
-        ds.add_param('bam', ds.params.get('bam_directory'))
+        ds.add_param("bam", ds.params.get("bam_directory"))
         ds.remove_param("bam_directory")
+        
+    ds.remove_param("input_type")
