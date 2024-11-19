62a3165b166f2f94d8a4424b380d6bf9f99f0f39
dltamayo <DLT@breakthroughcancer.org>
2024-11-19 14:25:00 -0500

    Functionize preprocess script

--- somatic_variation/preprocess.py
+++ somatic_variation/preprocess.py
@@ -1,22 +1,17 @@
 from cirro.helpers.preprocess_dataset import PreprocessDataset
 
+def process_input_type(ds, input_type, bam_file, bam_dir, bam):
+    if ds.params.get(input_type) == "file":
+        ds.add_param(bam, ds.params.get(bam_file))
+        ds.remove_param(bam_file)
+    elif ds.params.get(input_type) == "directory":
+        ds.add_param(bam, ds.params.get(bam_dir))
+        ds.remove_param(bam_dir)
+
+    ds.remove_param(input_type)
+
 if __name__ == "__main__":
     ds = PreprocessDataset.from_running()
-
-    if ds.params.get("input_type_tumor") == "file":
-        ds.add_param("bam_tumor", ds.params.get("bam_tumor_file"))
-        ds.remove_param("bam_tumor_file")
-    else:
-        ds.add_param("bam_tumor", ds.params.get("bam_tumor_directory"))
-        ds.remove_param("bam_tumor_directory")
-        
-    ds.remove_param("input_type_tumor")
-
-    if ds.params.get("input_type_normal") == "file":
-        ds.add_param("bam_normal", ds.params.get("bam_normal_file"))
-        ds.remove_param("bam_normal_file")
-    elif ds.params.get("input_type_normal") == "directory":
-        ds.add_param("bam_normal", ds.params.get("bam_normal_directory"))
-        ds.remove_param("bam_normal_directory")
-        
-    ds.remove_param("input_type_normal")
+    
+    process_input_type(ds, "input_type_tumor", "bam_tumor_file", "bam_tumor_directory", "bam_tumor")
+    process_input_type(ds, "input_type_normal", "bam_normal_file", "bam_normal_directory", "bam_normal")
