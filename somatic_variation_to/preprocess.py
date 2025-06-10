752f1206ddad20923a96a28ca4691d574647bfef
dltamayo <DLT@breakthroughcancer.org>
2025-06-10 09:59:25 -0400

    Init scdownstream

--- somatic_variation_to/preprocess.py
+++
@@ -1,7 +0,0 @@
-from cirro.helpers.preprocess_dataset import PreprocessDataset
-
-if __name__ == "__main__":
-    ds = PreprocessDataset.from_running()
-
-    # Remove param validation to override dss_threads limit
-    ds.add_param("validate_params", False)
