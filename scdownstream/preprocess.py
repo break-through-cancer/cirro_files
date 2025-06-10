913985e6936ee3e342d4c5a50b0b7bc6a1ca376e
dltamayo <DLT@breakthroughcancer.org>
2025-06-10 10:47:34 -0400

    Implement samplesheet

---
+++ scdownstream/preprocess.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from cirro.helpers.preprocess_dataset import PreprocessDataset
+import pandas as pd
+import numpy as np
+
+# 1. Get parameters from cirro pipeline call
+ds = PreprocessDataset.from_running()
+ds.logger.info("List of starting params")
+ds.logger.info(ds.params)
+
+ds.logger.info('checking ds.files')
+ds.logger.info(ds.files.head())
+ds.logger.info(ds.files.columns)
+
+# 2. Add samplesheet parameter and set equal to ds.samplesheet
+ds.logger.info("Checking samplesheet parameter")
+ds.logger.info(ds.samplesheet)
+samplesheet = ds.samplesheet
+samplesheet.rename(columns={"file":"filtered"})
+samplesheet.to_csv('samplesheet.csv', index=None)
+ds.add_param("input", "samplesheet.csv")
+
